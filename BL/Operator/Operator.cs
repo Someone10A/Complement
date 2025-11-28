65ee1ede650f456acbd18609ec6dbb8343adeeeb
Alan Oran <alanoranespinoza@gmail.com>
2025-11-28 15:49:06 -0600

    28112025

---
+++ BL/Operator/Operator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Operator
+{
+    public class Operator
+    {
+        //Metodo para obtener La ruta asignada, obvio aveces no hay ruta asignada.(Vista    Asignado)
+        /*Con base a ora_asignacion_operador el input es solo rfc_ope y mode
+           Devolver en ML.Result una clase */
+
+        //Metodo para obtener EL DETALLE de la ruta
+        /*
+         
+         */
+
+        //Metodo para obtener el historico de rutas Cerradas.(Vista Historico)
+        /*Con base a ora_asignacion_operador el input es solo rfc_ope y mode*/
+    }
+}
